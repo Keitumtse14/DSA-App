e7caaa7980f17809efcec195f1a9b76d748ed97c
Keitu <modipakeitumetse@gmail.com>
2023-02-05 09:09:59 +0200

    added testing coverage with c8

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    globals:true,
+    globals: true,
     coverage: {
       reporter: ['text', 'json', 'html'],
-  },
+    },
+  }
 })
